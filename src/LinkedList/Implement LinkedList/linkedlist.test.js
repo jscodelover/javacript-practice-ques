d0f4d3798f6479137a4c691ddc044caa80a9f1bc
jscodelover <manisha.4918@gmail.com>
2019-05-07 21:04:37 +0530

    feat: prepend test case

--- src/LinkedList/Implement LinkedList/linkedlist.test.js
+++ src/LinkedList/Implement LinkedList/linkedlist.test.js
@@ -4,7 +4,11 @@ describe("Test case for 1 implementation in linked list", () => {
   test("append test case", () => {
     MyLinkedList.append(5);
     MyLinkedList.append(15);
-    MyLinkedList.append(25);
-    expect(MyLinkedList.print()).toBe("10 --> 5 --> 15 --> 25");
+    expect(MyLinkedList.print()).toBe("10 --> 5 --> 15");
+  });
+  test("prepend test case", () => {
+    MyLinkedList.prepend(45);
+    MyLinkedList.prepend(1);
+    expect(MyLinkedList.print()).toBe("1 --> 45 --> 10 --> 5 --> 15");
   });
 });
